fe6eec8ef4514e1e87d956f1137ad1fdfa0b01dc
기은서 <ellen1987@naver.com>
2023-07-10 15:43:30 +0900

    test commit

---
+++ app/src/main/java/com/es/weatherprojects_tuk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.es.weatherprojects_tuk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
